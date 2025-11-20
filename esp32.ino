c16997801fa67283862fd809cf015e026a9daf35
gabspaludo <gpf.gabriel.gpf@gmail.com>
2025-11-20 19:41:30 -0300

    esp32 code

---
+++ esp32.ino
@@ -0,0 +1,58 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+const int LIMITE_PORTA = 20;
+
+#define TRIGGER_PIN D33
+#define ECHO_PIN D25
+
+long duration;
+int distance;
+bool estaAberta = false;
+bool ultimoEstadoAberta = false;
+
+long readUltrasonicDistance(int triggerPin, int echoPin) {
+  pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_GABRIEL_MARCELO_GONCALVES");
+  Serial.println("Bluetooth iniciado. Pronto para parear.");
+}
+
+void loop() {
+  duration = readUltrasonicDistance(TRIGGER_PIN, ECHO_PIN);
+  distance = duration * 0.0343 / 2;
+  
+  if (distance > LIMITE_PORTA) {
+    estaAberta = true;
+  } else {
+    estaAberta = false;
+  }
+
+  if (estaAberta != ultimoEstadoAberta) {
+    
+    if (estaAberta) {
+      SerialBT.println("ABERTA");
+      Serial.println("Mudou para: ABERTA"); 
+    } else {
+      SerialBT.println("FECHADA");
+      Serial.println("Mudou para: FECHADA");
+    }
+    
+    ultimoEstadoAberta = estaAberta;
+    
+    delay(500); 
+  }
+
+  delay(100); 
+}
